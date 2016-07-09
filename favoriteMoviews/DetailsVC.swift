cdfb3da212da7521f7da1efbcacb99375f4f42e1
=webmate <=>
2016-07-09 09:04:39 +0300

    DetailsVc
    Created the details Vc and the seague for it

---
+++ favoriteMoviews/DetailsVC.swift
@@ -0,0 +1,21 @@
+//
+//  DetailsVC.swift
+//  favoriteMoviews
+//
+//  Created by dimitris vlagas on 7/7/16.
+//  Copyright © 2016 webmate. All rights reserved.
+//
+
+import UIKit
+
+class DetailsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+   
+
+}
